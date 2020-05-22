f9019c11f485d14fc650cd56780f8c98be27c051
Manel Naspreda <manel.naspreda@Manels-MacBook-Pro.local>
2020-05-22 15:07:45 -0300

    Keeps more similar as the reactive homonomous

--- src/main/java/com/service/rest/users/UserRepository.java
+++ src/main/java/com/service/rest/users/UserRepository.java
@@ -1,6 +1,6 @@
 package com.service.rest.users;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<UserData, Long> {
+public interface UserRepository extends CrudRepository<UserData, Long> {
 }
